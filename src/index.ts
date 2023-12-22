9cd7aa79c3957737d11355f27aeff3f97ea0d3c3
Hyman.Hsu <hymanhsu8175@gmail.com>
2023-12-21 23:41:49 -0800

    add one custom loader to import module without extention '.js'

---
+++ src/index.ts
@@ -0,0 +1,31 @@
+// npx ts-node src/index.ts
+
+import express, { Express, Request, Response } from "express";
+import responseTime from 'response-time';
+import dotenv from "dotenv";
+import cors from 'cors';
+import helmet from 'helmet';
+import {heloRouter} from './api/helo/helo1';
+
+
+dotenv.config();
+
+const app: Express = express();
+
+// using the dependancies
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+app.use(responseTime());
+
+// setting routers
+app.use("/api/helo", heloRouter);
+
+
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log(`[server]: Server is running at http://localhost:${port}`);
+});
+
